1ccc2bf8db5baa95e17711e9f048ba6bd39d2ef3
Akif397 <akifmuhtasim397@gmail.com>
2021-06-10 13:15:44 +0600

    Ehcache v3 with redis

--- src/main/java/com/akif/demoforehcache/config/EhCacheConfiguration.java
+++ src/main/java/com/akif/demoforehcache/config/EhCacheConfiguration.java
@@ -1,6 +1,6 @@
 package com.akif.demoforehcache.config;
 
-import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
@@ -8,33 +8,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 
+
 @Configuration
+@EnableCaching
 public class EhCacheConfiguration {
-    @Primary
-    @Bean(name = "ehCacheCacheManager")
-    public EhCacheCacheManager ehCacheCacheManager(EhCacheManagerFactoryBean factoryBean) {
-        return new EhCacheCacheManager(factoryBean.getObject());
-    }
-
-    @Bean
-    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
-        EhCacheManagerFactoryBean cacheManagerFactoryBean = new EhCacheManagerFactoryBean();
-        cacheManagerFactoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
-        cacheManagerFactoryBean.setShared(true);
-        return cacheManagerFactoryBean;
-
-    }
-
-//    @Bean
-//    public CacheManager cacheManager() {
-//        return new EhCacheCacheManager(cacheMangerFactory().getObject());
+//    @Primary
+//    @Bean(name = "ehCacheCacheManager")
+//    public EhCacheCacheManager ehCacheCacheManager(EhCacheManagerFactoryBean factoryBean) {
+//        return new EhCacheCacheManager(factoryBean.getObject());
 //    }
 //
 //    @Bean
-//    public EhCacheManagerFactoryBean cacheMangerFactory() {
-//        EhCacheManagerFactoryBean bean = new EhCacheManagerFactoryBean();
-//        bean.setConfigLocation(new ClassPathResource("ehcache.xml"));
-//        bean.setShared(true);
-//        return bean;
+//    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
+//        EhCacheManagerFactoryBean cacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+//        cacheManagerFactoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
+//        cacheManagerFactoryBean.setShared(true);
+//        return cacheManagerFactoryBean;
+//
 //    }
 }
